eda01f87be999db4b7095417887efdb6672f7a93
daviidBuiitrago <158359585+daviidBuiitrago@users.noreply.github.com>
2024-02-09 14:21:43 -0500

    Add files via upload

---
+++ vars/dockerLib.groovy
@@ -0,0 +1,6 @@
+def build(Map params){
+    sh"docker build -f ${params.DockerfilePath} -t ${params.DockerImage} ${params.DockerContext}"
+}
+def push(Map params){
+    sh"docker push ${params.DockerImage}"
+}
